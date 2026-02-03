2c2b3afcff6af8e602d4511bed957368ca3fb5c9
ɓɛɲ <84660669+bengabp@users.noreply.github.com>
2026-02-03 14:06:55 +0100

    fix: use recode-video instead of forcing codec to avoid 403

--- m3dlp/tasks.py
+++ m3dlp/tasks.py
@@ -34,7 +34,7 @@ async def download_media(url, chat_id, message_id):
     cmd = [
         "yt-dlp",
         "-f", "bestvideo+bestaudio/best",
-        "-S", "vcodec:h264,res,acodec:m4a",
+        "--recode-video", "mp4",
         "--merge-output-format", "mp4",
         "-o", video_path,
         url
